4a0eb83033dc1b2a38f662c97ff6bf1af39d5027
dhruvb0812-sketch <dhruvb0812@gmail.com>
2026-03-14 19:49:30 +0530

    Update wheel.js

--- wheel.js
+++ wheel.js
@@ -1,54 +1,34 @@
 const { createCanvas } = require("canvas");
 
-async function generateWheel(players) {
+async function generateWheel(players){
 
-const size = 800;
+const size = 500;
 const canvas = createCanvas(size,size);
 const ctx = canvas.getContext("2d");
-
-const center = size/2;
-const radius = size/2;
 
 const angle = (2*Math.PI)/players.length;
 
 players.forEach((p,i)=>{
 
-const start = i*angle;
-const end = start+angle;
-
 ctx.beginPath();
-ctx.moveTo(center,center);
-ctx.arc(center,center,radius,start,end);
-
-ctx.fillStyle = i%2 ? "#ff7675":"#74b9ff";
-
+ctx.moveTo(size/2,size/2);
+ctx.arc(size/2,size/2,size/2,i*angle,(i+1)*angle);
+ctx.fillStyle=`hsl(${i*360/players.length},70%,60%)`;
 ctx.fill();
+ctx.stroke();
 
 ctx.save();
-
-ctx.translate(center,center);
-ctx.rotate(start+angle/2);
-
-ctx.fillStyle="black";
-ctx.font="24px Arial";
-
-ctx.fillText(p.name,150,0);
-
+ctx.translate(size/2,size/2);
+ctx.rotate(i*angle+angle/2);
+ctx.fillStyle="#000";
+ctx.font="20px Arial";
+ctx.fillText(p.name,150,10);
 ctx.restore();
 
 });
-
-ctx.beginPath();
-
-ctx.moveTo(center-20,20);
-ctx.lineTo(center+20,20);
-ctx.lineTo(center,70);
-
-ctx.fillStyle="red";
-ctx.fill();
 
 return canvas.toBuffer();
 
 }
 
-module.exports = { generateWheel };
+module.exports={generateWheel};
